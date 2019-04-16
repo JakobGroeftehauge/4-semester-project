fa2213c713b96dee2aeaaf9d5174500a58a8af2e
Kaadx7 <jmkaad@live.dk>
2019-04-16 10:26:27 +0200

    N

--- C libraries/PID_MA.c
+++ C libraries/PID_MA.c
@@ -9,7 +9,7 @@
 * YYMMDD
 * --------------------
 * 190408  JGR    Module created.
-* 190409  PC	 Revised.
+* 190409  PC     Revised.
 *
 *****************************************************************************/
 #include "PID_MA.h"
@@ -38,18 +38,10 @@ extern void PID_task(uint8_t id, uint8_t state, uint8_t event, uint8_t data)
 {
    float referencePoint = 10;
    float feedback;
-<<<<<<< HEAD
-   INT16U POS_data;
-   int16_t result_to_send = 0;
-=======
-   uint16_t POS_data;
-   int16_t result_to_send;
->>>>>>> 7d38a106456e5c3f5836993ebb04c101f0cd7af1
 
-   wait_sem( SEM_POS_UPDATE, WAIT_FOREVER );
-   if( event != EVENT_RESET )
-   {
-<<<<<<< HEAD
+   int16_t result_to_send;
+
+
         feedback = pos_var;
         feedback = 9.9;
         float result_PID = run_PID(feedback, referencePoint, CC_CONTROLLER_ID);
@@ -62,17 +54,6 @@ extern void PID_task(uint8_t id, uint8_t state, uint8_t event, uint8_t data)
 //       while( !uart0_tx_rdy() )
 //       {}
 //       uart0_putc(data_LOW);
-
-=======
-       if(get_queue( Q_SPI_POS, &POS_data, WAIT_FOREVER ) )
-       {
-           feedback = POS_data*1;
-           float result_PID = run_PID(feedback, referencePoint, CC_CONTROLLER_ID);
-           result_to_send = voltage_to_duty_cycle(result_PID);
-           put_queue(Q_SPI_PWM, result_to_send, WAIT_FOREVER);// put result to queue
-       }
->>>>>>> 7d38a106456e5c3f5836993ebb04c101f0cd7af1
-
         pwm_var = result_PID;
 
 
@@ -97,14 +78,14 @@ extern void init_PIDs()
     PID_pool[CC_CONTROLLER_ID].upper_sat = 10;
     PID_pool[CC_CONTROLLER_ID].lower_sat = -10;
     PID_pool[CC_CONTROLLER_ID].filter_id = CC_CONTROLLER_ID;
-	PID_pool[CC_CONTROLLER_ID].pastError = 0;
-	PID_pool[CC_CONTROLLER_ID].Ud = 0;
-	PID_pool[CC_CONTROLLER_ID].sat_flag = 0;
+    PID_pool[CC_CONTROLLER_ID].pastError = 0;
+    PID_pool[CC_CONTROLLER_ID].Ud = 0;
+    PID_pool[CC_CONTROLLER_ID].sat_flag = 0;
     float CC_Filter_Coef[MAX_NUMBER_OF_TABS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     init_filter(CC_CONTROLLER_ID, CC_Filter_Coef, 3);
     
-	
-	//SETUP FILTER
+
+    //SETUP FILTER
 
     //Setup of the Velocity Controller
 
@@ -126,44 +107,44 @@ extern float run_PID(float feedback, float setpoint, uint8_t id) // CHANGE TO PI
    float output;
    float T = PID_pool[id].dt;
    float integral_term;
-	
-	error = setpoint - feedback;
 
-	// calculate the proportional and derivative terms
-	float proportional_term  = PID_pool[id].Kp*error;
-	float derivative_term = PID_pool[id].Kd*2/T*(error - PID_pool[id].previous_error)-PID_pool[id].Ud;
-	
-	// integral is only given a value if the controller is not in saturation
-	if (PID_pool[id].sat_flag)
-	{
-		integral_term = 0;
-	}
-	else
-	{
-		integral_term = PID_pool[id].integral + PID_pool[id].Ki*T/2*(error + PID_pool[id].previous_error);
-	}
-	
-	output = proportional_term + integral_term + derivative_term;
-	
-	PID_pool[id].integral = integral_term;
-	PID_pool[id].Ud = derivative_term;
-	
-	// check for saturation for next run through and set flag 
-	if (output > PID_pool[id].upper_sat)
-	{
-		output = PID_pool[id].upper_sat;
-		PID_pool[id].sat_flag = 1;
-	}
-	else if (output < PID_pool[id].lower_sat)
-	{
-		output = PID_pool[id].lower_sat;
-		PID_pool[id].sat_flag = 1;
-	}
-	else
-	{
-		PID_pool[id].sat_flag = 0;
-	}
-	
-	return output;
+    error = setpoint - feedback;
+
+    // calculate the proportional and derivative terms
+    float proportional_term  = PID_pool[id].Kp*error;
+    float derivative_term = PID_pool[id].Kd*2/T*(error - PID_pool[id].previous_error)-PID_pool[id].Ud;
+
+    // integral is only given a value if the controller is not in saturation
+    if (PID_pool[id].sat_flag)
+    {
+        integral_term = 0;
+    }
+    else
+    {
+        integral_term = PID_pool[id].integral + PID_pool[id].Ki*T/2*(error + PID_pool[id].previous_error);
+    }
+
+    output = proportional_term + integral_term + derivative_term;
+
+    PID_pool[id].integral = integral_term;
+    PID_pool[id].Ud = derivative_term;
+
+    // check for saturation for next run through and set flag
+    if (output > PID_pool[id].upper_sat)
+    {
+        output = PID_pool[id].upper_sat;
+        PID_pool[id].sat_flag = 1;
+    }
+    else if (output < PID_pool[id].lower_sat)
+    {
+        output = PID_pool[id].lower_sat;
+        PID_pool[id].sat_flag = 1;
+    }
+    else
+    {
+        PID_pool[id].sat_flag = 0;
+    }
+
+    return output;
 
 }

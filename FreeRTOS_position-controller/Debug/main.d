048c0c4eba39d56ae346d32ce9a6cfaa2e7850c8
Kaadx7 <jmkaad@live.dk>
2019-04-24 08:49:43 +0200

    Revert "Revert "mads""
    This reverts commit 00114ffa8ba1e0014c2fbfd92abd1cbd413e9c24.

--- FreeRTOS_position-controller/Debug/main.d
+++ FreeRTOS_position-controller/Debug/main.d
@@ -1,71 +1,71 @@
 main.o: ../main.c ../EMP_type.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/defines.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/EMP_type.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/portmacro.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/semphr.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/queue.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/PID_FreeRTOS.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/filter.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/circular_buffer.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/emp_type.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/defines.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/EMP_type.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/portmacro.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/semphr.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/queue.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/PID_FreeRTOS.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/filter.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/circular_buffer.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/emp_type.h \
  ../tm4c123gh6pm.h ../gpio.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/SPI.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/tm4c123gh6pm.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/PID_FreeRTOS.h
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/SPI.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/tm4c123gh6pm.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/PID_FreeRTOS.h
 
 ../EMP_type.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/defines.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/defines.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/EMP_type.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/EMP_type.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/portmacro.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/portmacro.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/semphr.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/semphr.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/queue.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/queue.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/PID_FreeRTOS.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/PID_FreeRTOS.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/filter.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/filter.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/circular_buffer.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/circular_buffer.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/emp_type.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/emp_type.h:
 
 ../tm4c123gh6pm.h:
 
 ../gpio.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/SPI.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/SPI.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/tm4c123gh6pm.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/tm4c123gh6pm.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/C\ libraries/PID_FreeRTOS.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/C\ libraries/PID_FreeRTOS.h:
--- FreeRTOS_position-controller/Debug/tasks.d
+++ FreeRTOS_position-controller/Debug/tasks.d
@@ -1,37 +1,37 @@
 tasks.o: ../tasks.c \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/portmacro.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/timers.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
- C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/stack_macros.h
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/portmacro.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/timers.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h \
+ C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/stack_macros.h
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/FreeRTOS.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/FreeRTOSConfig.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/projdefs.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/portable.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/deprecated_definitions.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOS_position-controller/portmacro.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOS_position-controller/portmacro.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/mpu_wrappers.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/list.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/timers.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/timers.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/task.h:
 
-C:/Users/Jakob/Google\ Drive/01\ SDU/04\ 4.\ Semester/05\ 4.\ semesterprojekt/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/stack_macros.h:
+C:/Users/Mads/Documents/GitHub/4-semester-project/FreeRTOSv10.2.0/FreeRTOS/Source/include/stack_macros.h:

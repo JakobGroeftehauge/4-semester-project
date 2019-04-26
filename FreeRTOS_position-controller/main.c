f11f6119c132779a8561021fc16d1ee939d51191
Kaadx7 <jmkaad@live.dk>
2019-04-26 14:47:39 +0200

    Not compiling

--- FreeRTOS_position-controller/main.c
+++ FreeRTOS_position-controller/main.c
@@ -70,7 +70,7 @@ int main(void)
     xTaskCreate(PID_PC_task, "Position controller 1", 100, &PC_1_parameter, 8, &PC_PID1_handle);
     xTaskCreate(PID_VC_task, "Velocity controller 1", 100, &VC_1_parameter, 8, &VC_PID1_handle);
     xTaskCreate(PID_PC_task, "Position controller 2", 100, &PC_2_parameter, 8, &PC_PID2_handle);
-    xTaskCreate(PID_PC_task, "Velocity controller 2", 100, &VC_2_parameter, 8, &VC_PID2_handle);
+    xTaskCreate(PID_VC_task, "Velocity controller 2", 100, &VC_2_parameter, 8, &VC_PID2_handle);
 
     xTaskCreate(SPI_task, "SPI module", 100, 4, 1, &SPI_handle);
 

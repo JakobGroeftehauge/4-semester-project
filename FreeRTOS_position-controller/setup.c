f12f8700eedcd59b6b29639e86f26dc30b6a6290
Kaadx7 <jmkaad@live.dk>
2019-04-27 14:53:03 +0200

    setup init_parameters done

--- FreeRTOS_position-controller/setup.c
+++ FreeRTOS_position-controller/setup.c
@@ -21,7 +21,7 @@ int16_t control_2_cur_ref;
 int16_t glob_protocol;
 
 /****************************    Semaphores    ***************************/
-volatile SemaphoreHandle_t QUEUE_SEM;
+QUEUE_SEM;
 
 extern void init_sem()
 /*****************************************************************************
@@ -68,51 +68,41 @@ extern void init_parameters()
 *   Function : -
 ******************************************************************************/
 {
-     struct PID_parameter PC_1_parameter =
-     {
-     PC_CONTROLLER_1_ID,// id
-     POS_1,// slave_id
-     &control_1_vel_ref, // *place to store output
-     &VEL_1_REF_SEM, // *output semaphore
-     &control_1_pos_ref, // *reference_signal
-     &POS_1_REF_SEM, // *reference_semaphore
-     &control_1_pos, // *feedback_signal
-     &POS_1_SEM, // *feedback_semaphore
-     20, // delay time (ms)
-     &QUEUE_SEM, // *queue_semaphore
-     0// output id
-    };
+    PC_2_parameter.id = PC_CONTROLLER_1_ID;
+    PC_2_parameter.slave_id = POS_1;
+    PC_2_parameter.place_to_store_output = &control_1_vel_ref;
+    PC_2_parameter.output_semaphore = &VEL_1_REF_SEM;
+    PC_2_parameter.reference_signal = &control_1_pos_ref;
+    PC_2_parameter.reference_semaphore = &POS_1_REF_SEM;
+    PC_2_parameter.feedback_signal =  &control_1_pos;
+    PC_2_parameter.feedback_semaphore =  &POS_1_SEM;
+    PC_2_parameter.delayTime = 20;
+    PC_2_parameter.queue_semaphore = &QUEUE_SEM;
+    PC_2_parameter.output_id = 0;
 
+    VC_2_parameter.id = VC_CONTROLLER_1_ID;
+    VC_2_parameter.slave_id = VEL_1;
+    VC_2_parameter.place_to_store_output = &control_1_vel_ref;
+    VC_2_parameter.output_semaphore = &VEL_1_REF_SEM;
+    VC_2_parameter.reference_signal = &control_1_vel_ref;
+    VC_2_parameter.reference_semaphore = &VEL_1_REF_SEM;
+    VC_2_parameter.feedback_signal = &control_1_vel;
+    VC_2_parameter.feedback_semaphore = &VEL_1_SEM;
+    VC_2_parameter.delayTime = 5;
+    VC_2_parameter.queue_semaphore = &QUEUE_SEM;
+    VC_2_parameter.output_id = 0;
 
-    struct PID_parameter VC_1_parameter =
-    {
-     VC_CONTROLLER_1_ID,// id
-     VEL_1,// slave_id
-     &control_1_vel_ref, // *place to store output (BRUGES IKKE)
-     &VEL_1_REF_SEM, // *output semaphore (BRUGES IKKE)
-     &control_1_vel_ref, // *reference_signal
-     &VEL_1_REF_SEM, // *reference_semaphore
-     &control_1_vel, // *feedback_signal
-     &VEL_1_SEM, // *feedback_semaphore
-     5, // delay time (ms)
-     &QUEUE_SEM, // *queue_semaphore
-     0// output id
-    };
-
-    struct PID_parameter PC_2_parameter =
-    {
-     PC_CONTROLLER_2_ID,// id
-     POS_2,// slave_id
-     &control_2_vel_ref, // *place to store output
-     &VEL_2_REF_SEM, // *output semaphore
-     &control_2_pos_ref, // *reference_signal
-     &POS_2_REF_SEM, // *reference_semaphore
-     &control_2_pos, // *feedback_signal
-     &POS_2_SEM, // *feedback_semaphore
-     20, // delay time (ms)
-     &QUEUE_SEM, // *queue_semaphore
-     0// output id
-    };
+    PC_2_parameter.id = PC_CONTROLLER_2_ID;
+    PC_2_parameter.slave_id = POS_2;
+    PC_2_parameter.place_to_store_output = &control_2_vel_ref;
+    PC_2_parameter.output_semaphore = &VEL_2_REF_SEM;
+    PC_2_parameter.reference_signal = &control_2_pos_ref;
+    PC_2_parameter.reference_semaphore = &POS_2_REF_SEM;
+    PC_2_parameter.feedback_signal =  &control_2_pos;
+    PC_2_parameter.feedback_semaphore =  &POS_2_SEM;
+    PC_2_parameter.delayTime = 20;
+    PC_2_parameter.queue_semaphore = &QUEUE_SEM;
+    PC_2_parameter.output_id = 0;
 
     VC_2_parameter.id = VC_CONTROLLER_2_ID;
     VC_2_parameter.slave_id = VEL_2;
